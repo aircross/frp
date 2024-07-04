842782f5c55bd8217fc19fa8be79286e02166c40
aircross <aircross@users.noreply.github.com>
2024-07-04 22:26:54 +0800

    fix

--- client/admin_api.go
+++ client/admin_api.go
@@ -181,7 +181,7 @@ func (svr *Service) apiStatus(w http.ResponseWriter, _ *http.Request) {
 
 	ps := ctl.pm.GetAllProxyStatus()
 	for _, status := range ps {
-		serverIP, err := netpkg.GetDomainIP(c.ServerAddr)
+		serverIP, err := netpkg.GetDomainIP(svr.common.ServerAddr)
 		fmt.Println(err)
 		res[status.Type] = append(res[status.Type], NewProxyStatusResp(status, serverIP))
 	}
